c16033fda97808a2291d5c923bda23e770540006
Lucas Josino <contact@lucasjosino.com>
2022-08-22 20:58:51 -0300

    WIP: Home page

---
+++ src/constants/linkBox.ts
@@ -0,0 +1,8 @@
+interface LinkBoxConfig {
+  height: string;
+  width: string;
+}
+
+export const linkBox: LinkBoxConfig = { height: '50px', width: '11.2rem' };
+export const social: LinkBoxConfig = { height: '70px', width: '5rem' };
+export const project: LinkBoxConfig = { height: '140px', width: '11.2rem' };
