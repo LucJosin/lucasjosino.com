b189ab14985acb1108fcc0a4e30f82ae1c4f0e28
Lucas Josino <contact@lucasjosino.com>
2022-08-22 10:02:24 -0300

    Setup layout (#1)
    - Add `astrojs/image`;
    - Update tsconfig(paths);
    - Add background image (static/image);
    - Remove Card component;
    - Setup layout (head, description, title, SEO, etc...).

---
+++ src/constants/head.ts
@@ -0,0 +1,15 @@
+interface HeadConfig {
+  name: string;
+  titleStart: string;
+  endDescription: string;
+  tags: string;
+}
+
+const headConfig: HeadConfig = {
+  name: 'Lucas Josino',
+  titleStart: 'Lucas Josino | ',
+  endDescription: '• lucasjosino.com | lucjos.in | @lucjosin',
+  tags: 'flutter, flutter developer, dart, kotlin, kotlin developer, android, android developer, portfolio, lucasjosino, lucas josino, lucjosin, lucjos.in, josino, mobile, developer, mobile developer, @lucjoin, @lucjos.in, desenvolvedor, desenvolvedor android, desenvolvedor flutter, desenvolvedor kotlin',
+};
+
+export default headConfig;
