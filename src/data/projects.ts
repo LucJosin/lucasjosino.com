f7eb5f1c95cf06a5da1991d65c8c6ab903b8cec9
Lucas Josino <contact@lucasjosino.com>
2022-08-23 15:33:58 -0300

    Update home page.
    - Add links;
    - Fix dark mode;

---
+++ src/data/projects.ts
@@ -0,0 +1,14 @@
+export const projects = [
+  {
+    name: 'Github',
+    icon: 'github',
+    user: 'LucJosin',
+    url: 'https://github.com/LucJosin',
+  },
+  {
+    name: 'Linkedin',
+    icon: 'linkedin',
+    user: 'LucJosin',
+    url: 'https://linkedin.com/in/LucJosin',
+  },
+];
