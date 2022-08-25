855b1846d0f73a5f22d67422604419bec1512a01
Lucas Josino <contact@lucasjosino.com>
2022-08-25 20:16:07 -0300

    Add more mobile support

--- src/constants/linkBox.ts
+++ src/constants/linkBox.ts
@@ -3,6 +3,6 @@ interface LinkBoxConfig {
   width: string;
 }
 
-export const linkBox: LinkBoxConfig = { height: '50px', width: '11.2rem' };
-export const social: LinkBoxConfig = { height: '70px', width: '5rem' };
-export const project: LinkBoxConfig = { height: '140px', width: '11.2rem' };
+export const linkBox: LinkBoxConfig = { height: '3.1rem', width: '11.2rem' };
+export const social: LinkBoxConfig = { height: '4.3rem', width: '5rem' };
+export const project: LinkBoxConfig = { height: '8.75rem', width: '11.2rem' };
