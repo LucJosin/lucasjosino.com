6048d34ddf63239d17ae9ded1069ad430516eae3
Lucas Josino <contact@lucasjosino.com>
2022-08-29 22:12:43 -0300

    WIP: Centralize informations

--- src/constants/linkBox.ts
+++
@@ -1,8 +0,0 @@
-interface LinkBoxConfig {
-  height: string;
-  width: string;
-}
-
-export const linkBox: LinkBoxConfig = { height: '3.1rem', width: '11.2rem' };
-export const social: LinkBoxConfig = { height: '4.3rem', width: '5rem' };
-export const project: LinkBoxConfig = { height: '8.75rem', width: '11.2rem' };
