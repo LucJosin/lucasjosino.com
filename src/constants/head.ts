0705353b15873a2e0eb83d2cd8baa511ca0c7168
Lucas Josino <contact@lucasjosino.com>
2023-08-15 13:18:01 -0300

    fix: Image path

--- src/constants/head.ts
+++ src/constants/head.ts
@@ -18,8 +18,8 @@ const headConfig: HeadConfig = {
   copyRight: '© 2023, Lucas Josino. All Rights Reserved.',
   endDescription: '• lucasjosino.com | lucasjosino.dev | lucjos.in | @lucjosin',
   tags: 'java, java developer, kotlin, sql, postgresql, hawapi, spring, spring boot, spring mvc, kotlin developer, backend, backend developer, portfolio, lucasjosino, lucas josino, lucjosin, lucjos.in, josino, developer, @lucjoin, @lucjos.in, desenvolvedor, desenvolvedor backend, desenvolvedor kotlin',
-  icon: './favicon.ico',
-  banner: './static/banner.jpg',
+  icon: '/favicon.ico',
+  banner: '/static/banner.jpg',
   robots: 'index,follow',
   canonical: 'https://lucasjosino.dev',
 };
