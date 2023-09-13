d77495bf83fd715b72aabb6813ecb53db1f7edcf
Lucas Josino <contact@lucasjosino.com>
2023-09-12 22:07:20 -0300

    feat: Update projects constants

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,10 +1,25 @@
 export const projects = [
   {
+    title: 'Hawbrary',
+    description:
+      'The Hawbrary is a showcase of the HawAPI - A Free and Open Source API for Stranger Things - built with React (NextJs) + Typescript.',
+    alt: 'Homepage',
+    git: 'https://github.com/LucJosin/hawbrary',
+    url: 'https://hawbrary.theproject.id/',
+  },
+  {
     title: 'HawAPI',
-    description: 'A Free and Open Source API for Stranger Things series.',
+    description:
+      'A Free and Open Source API for Stranger Things built with Java (Spring Boot) and PostgreSQL with support for multiple languages (English and Portuguese).',
     alt: 'Homepage',
     git: 'https://github.com/HawAPI/HawAPI',
     url: 'https://hawapi.theproject.id/',
+  },
+  {
+    title: 'lucasjosino.com',
+    description: 'Personal website built with Astro & Typescript.',
+    alt: 'Homepage',
+    git: 'https://github.com/LucJosin/lucasjosino.com',
   },
   {
     title: 'on_audio_query',
