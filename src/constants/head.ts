255617d8967864ef2e03e17314da4f0b13e9f37c
Lucas Josino <contact@lucasjosino.com>
2023-09-15 11:22:14 -0300

    refactor: Update copyright metadata name

--- src/constants/head.ts
+++ src/constants/head.ts
@@ -2,7 +2,7 @@ interface HeadConfig {
   name: string;
   titleStart: string;
   social: string;
-  copyRight: string;
+  copyright: string;
   endDescription: string;
   tags: string;
   banner: string;
@@ -15,7 +15,7 @@ const headConfig: HeadConfig = {
   name: 'Lucas Josino',
   titleStart: 'Lucas Josino | ',
   social: '@LucJosin',
-  copyRight: '© 2023, Lucas Josino. All Rights Reserved.',
+  copyright: '© 2023, Lucas Josino. All Rights Reserved.',
   endDescription: '• lucasjosino.com | lucasjosino.dev | lucjos.in | @lucjosin',
   tags: 'java, java developer, kotlin, sql, postgresql, hawapi, spring, spring boot, spring mvc, kotlin developer, backend, backend developer, portfolio, lucasjosino, lucas josino, lucjosin, lucjos.in, josino, developer, @lucjoin, @lucjos.in, desenvolvedor, desenvolvedor backend, desenvolvedor kotlin',
   banner: 'static/banner.png',
