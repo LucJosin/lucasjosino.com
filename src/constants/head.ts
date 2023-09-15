5b50261b99ee9e68cac5739be2db5a4baf6ba244
Lucas Josino <contact@lucasjosino.com>
2023-09-15 19:58:53 -0300

    Update v1.3.0 (#17)
    Features
    
    - Add better support for mobile
    - Add /projects page
    - Add ChildrenProjectModel
    - Add alt to ProjectItem image
    - Add maxLine to Description component
    - Add isLocal to LinkBox usages
    - Add align param to Description component
    - Add shortlink to head metadata
    
    Changes
    
    - Update dependencies
    - Update metadata
    - Update projects constants
    - Update pages
    - Update components
    - Update root css variables
    - Update link components
    - Update css and html names
    - Rename MenuContainer to Container
    - Rename from Button to Link
    - Remove unnecessary css
    - Remove unused css and
    - Convert link component into simple link
    - Move layout folder
    
    Fixes
    
    - Fix wrong site url on layout metadata

--- src/constants/head.ts
+++ src/constants/head.ts
@@ -2,26 +2,28 @@ interface HeadConfig {
   name: string;
   titleStart: string;
   social: string;
-  copyRight: string;
+  copyright: string;
   endDescription: string;
   tags: string;
-  icon: string;
   banner: string;
   robots: string;
+  shortLink: string;
   canonical: string;
+  alternative: string;
 }
 
 const headConfig: HeadConfig = {
   name: 'Lucas Josino',
   titleStart: 'Lucas Josino | ',
   social: '@LucJosin',
-  copyRight: '© 2023, Lucas Josino. All Rights Reserved.',
+  copyright: '© 2023, Lucas Josino. All Rights Reserved.',
   endDescription: '• lucasjosino.com | lucasjosino.dev | lucjos.in | @lucjosin',
   tags: 'java, java developer, kotlin, sql, postgresql, hawapi, spring, spring boot, spring mvc, kotlin developer, backend, backend developer, portfolio, lucasjosino, lucas josino, lucjosin, lucjos.in, josino, developer, @lucjoin, @lucjos.in, desenvolvedor, desenvolvedor backend, desenvolvedor kotlin',
-  icon: '/favicon.ico',
-  banner: '/static/banner.png',
+  banner: 'static/banner.png',
   robots: 'index,follow',
-  canonical: 'https://lucasjosino.dev',
+  shortLink: 'https://lucjos.in',
+  canonical: 'https://lucasjosino.com',
+  alternative: 'https://lucasjosino.dev',
 };
 
 export default headConfig;
