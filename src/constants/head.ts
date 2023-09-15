4f44648ac35216044ec71d4c20b262081c919e59
Lucas Josino <contact@lucasjosino.com>
2023-09-15 16:08:20 -0300

    feat: Add shortlink to head metadata

--- src/constants/head.ts
+++ src/constants/head.ts
@@ -7,6 +7,7 @@ interface HeadConfig {
   tags: string;
   banner: string;
   robots: string;
+  shortLink: string;
   canonical: string;
   alternative: string;
 }
@@ -20,6 +21,7 @@ const headConfig: HeadConfig = {
   tags: 'java, java developer, kotlin, sql, postgresql, hawapi, spring, spring boot, spring mvc, kotlin developer, backend, backend developer, portfolio, lucasjosino, lucas josino, lucjosin, lucjos.in, josino, developer, @lucjoin, @lucjos.in, desenvolvedor, desenvolvedor backend, desenvolvedor kotlin',
   banner: 'static/banner.png',
   robots: 'index,follow',
+  shortLink: 'https://lucjos.in',
   canonical: 'https://lucasjosino.com',
   alternative: 'https://lucasjosino.dev',
 };
