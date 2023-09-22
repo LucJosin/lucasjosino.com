fc8da836888a00b292857ac8c54c4640828aba30
Lucas Josino <contact@lucasjosino.com>
2023-09-22 17:05:28 -0300

    feat: Update blog schema
    - Add `refFile`
    - Add default image
    - Add max of 65 to title
    - Add max of 160 to description
    - Add default shortlink

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -1,15 +1,16 @@
 import { reference, z } from 'astro:content';
 
 export const blogSchema = z.object({
-  image: z.string().optional(),
-  published_at: z.string(),
-  updated_at: z.string(),
-  title: z.string(),
-  description: z.string(),
+  refFile: z.string().optional(),
+  image: z.string().default('/static/banner.png'),
+  publishedAt: z.string(),
+  updatedAt: z.string(),
+  title: z.string().max(65),
+  description: z.string().max(160),
   tags: z.array(z.string()),
   category: z.string(),
   language: z.enum(['en', 'pt']),
-  shortlink: z.string(),
+  shortlink: z.string().default('/blog'),
   isVisible: z.boolean().default(true),
   enableComments: z.boolean().default(true),
   relatedPosts: z.array(reference('blog')).optional(),
