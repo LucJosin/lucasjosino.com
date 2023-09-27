3e640cd1fc06becb50f415750af4d92a83aad7e0
Lucas Josino <contact@lucasjosino.com>
2023-09-26 21:29:11 -0300

    feat: Add getAllPosts method

---
+++ src/lib/blog.ts
@@ -0,0 +1,15 @@
+import { getCollection } from 'astro:content';
+
+export async function getAllPosts(sortByDate: boolean = true) {
+  const posts = await getCollection('blog', (post) => {
+    return !post.data.isDraft && !post.data.isVisible;
+  });
+
+  if (sortByDate) {
+    posts.sort(
+      (a, b) => Date.parse(a.data.publishedAt) - Date.parse(b.data.publishedAt)
+    );
+  }
+
+  return posts;
+}
