7b9e1258da62f3e98a4db474d5a0efe78f40c1db
Lucas Josino <contact@lucasjosino.com>
2023-09-28 21:54:45 -0300

    feat: Update page descriptions

--- src/constants/head.ts
+++ src/constants/head.ts
@@ -15,7 +15,8 @@ const headConfig: HeadConfig = {
   name: 'Lucas Josino',
   social: '@LucJosin',
   copyright: '© 2023, Lucas Josino. All Rights Reserved.',
-  endDescription: '• lucasjosino.com | lucasjosino.dev | lucjos.in | @lucjosin',
+  endDescription:
+    ' • lucasjosino.com | lucasjosino.dev | lucjos.in | @lucjosin',
   tags: 'java, java developer, kotlin, sql, postgresql, hawapi, spring, spring boot, spring mvc, kotlin developer, backend, backend developer, portfolio, lucasjosino, lucas josino, lucjosin, lucjos.in, josino, developer, @lucjoin, @lucjos.in, desenvolvedor, desenvolvedor backend, desenvolvedor kotlin',
   banner: 'static/banner.png',
   robots: 'index,follow',
