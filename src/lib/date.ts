87723068908d8ca944a6d2c6e5720bca76b9bbd6
Lucas Josino <contact@lucasjosino.com>
2023-10-05 20:08:39 -0300

    refactor: Remove workaround for date

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,5 +1,5 @@
 export function getFormatedDate(date: string) {
-  return new Date(date + 'GMT-0700').toLocaleDateString('en-US', {
+  return new Date(date).toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
