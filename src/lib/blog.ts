ccf5732cc7fce75b02ba6000d96666dc22b1becf
Lucas Josino <contact@lucasjosino.com>
2023-10-04 22:53:13 -0300

    feat: Add ignoreNonVisiblePosts to getAllPosts

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,14 +1,26 @@
 import { getCollection } from 'astro:content';
 
-export async function getAllPosts(sortByDate: boolean = true) {
-  const posts = await getCollection('blog', (post) => {
-    return !post.data.isDraft && post.data.isVisible;
+export async function getAllPosts(
+  sortByDate: boolean = true,
+  ignoreNonVisiblePosts: boolean = true
+) {
+  let posts = await getCollection('blog', (post) => {
+    return !post.data.isDraft;
   });
 
+  if (ignoreNonVisiblePosts) {
+    posts = posts.filter((post) => {
+      return post.data.isVisible;
+    });
+  }
+
   if (sortByDate) {
-    posts.sort(
-      (a, b) => Date.parse(a.data.publishedAt) - Date.parse(b.data.publishedAt)
-    );
+    posts
+      .sort(
+        (a, b) =>
+          Date.parse(a.data.publishedAt) - Date.parse(b.data.publishedAt)
+      )
+      .reverse();
   }
 
   return posts;
