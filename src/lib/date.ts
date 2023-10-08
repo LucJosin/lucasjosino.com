f8754f8e161a1d1e8a777de59f5f922d115354a6
Lucas Josino <contact@lucasjosino.com>
2023-10-08 12:14:44 -0300

    feat: Add `time` param to getFormatedDate

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,7 +1,16 @@
-export function getFormatedDate(date: string) {
-  return new Date(date).toLocaleDateString('en-US', {
+export function getFormatedDate(date: string, time: boolean = false) {
+  const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
-  });
+  };
+
+  if (time) {
+    options.hour = 'numeric';
+    options.minute = 'numeric';
+    options.second = 'numeric';
+    options.hour12 = false;
+  }
+
+  return new Date(date).toLocaleDateString('en-US', options);
 }
