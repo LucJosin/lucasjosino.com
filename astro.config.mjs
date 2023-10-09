79a9820a868d9010463b7cb9a2e7a99fea28cb37
Lucas Josino <contact@lucasjosino.com>
2023-10-09 11:58:33 -0300

    feat: Add rel, target and icon to external links

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 import { s } from 'hastscript';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeExternalLinks from 'rehype-external-links';
 import rehypeSlug from 'rehype-slug';
 import remarkCollapse from 'remark-collapse';
 import numberedFootnoteLabels from 'remark-numbered-footnote-labels';
@@ -48,6 +49,29 @@ export default defineConfig({
   markdown: {
     rehypePlugins: [
       rehypeSlug,
+      [
+        rehypeExternalLinks,
+        {
+          rel: 'noopener noreferrer',
+          target: '_blank',
+          content: [
+            s(
+              'svg.external-links',
+              {
+                xmlns: 'http://www.w3.org/2000/svg',
+                width: 16,
+                height: 16,
+                marginLeft: '1rem',
+                fill: 'currentColor',
+                viewBox: '0 0 24 24',
+              },
+              s('path', {
+                d: 'M14 3v2h3.59l-9.83 9.83l1.41 1.41L19 6.41V10h2V3m-2 16H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7Z',
+              })
+            ),
+          ],
+        },
+      ],
       [
         rehypeAutolinkHeadings,
         {
