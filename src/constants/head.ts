2d9595b8877879f31987586c911055a1c089103f
Lucas Josino <contact@lucasjosino.com>
2024-02-24 21:21:33 -0300

    feat: Add new og tags and Update metadata

--- src/constants/head.ts
+++ src/constants/head.ts
@@ -14,10 +14,10 @@ interface HeadConfig {
 const headConfig: HeadConfig = {
   name: 'Lucas Josino',
   social: '@LucJosin',
-  copyright: '© 2023, Lucas Josino. All Rights Reserved.',
+  copyright: '© 2024, Lucas Josino. All Rights Reserved.',
   endDescription:
     ' • lucasjosino.com | lucasjosino.dev | lucjos.in | @lucjosin',
-  tags: 'java, java developer, kotlin, sql, postgresql, hawapi, spring, spring boot, spring mvc, kotlin developer, backend, backend developer, portfolio, lucasjosino, lucas josino, lucjosin, lucjos.in, josino, developer, @lucjoin, @lucjos.in, desenvolvedor, desenvolvedor backend, desenvolvedor kotlin',
+  tags: 'java, java developer, kotlin, golang, go, sql, postgresql, hawapi, spring, spring boot, spring mvc, backend, backend developer, portfolio, lucasjosino, lucas josino, lucjosin, lucjos.in, josino, developer, @lucjoin, @lucjos.in, desenvolvedor, desenvolvedor backend, desenvolvedor java',
   banner: 'static/banner.png',
   robots: 'index,follow',
   shortLink: 'https://lucjos.in',
