4ef07d5eae8c844434076f0c0dd5e1d2de352b1d
Lucas Josino <contact@lucasjosino.com>
2024-05-17 20:41:07 -0300

    feat: update ProjectCard layout and responsiveness

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -9,14 +9,6 @@ export interface ProjectModel {
   language: string;
   icon: string;
   category: 'Project' | 'Collection';
-  children?: ChildrenProjectModel[];
-}
-
-interface ChildrenProjectModel {
-  title: string;
-  description: string;
-  git?: string;
-  url?: string;
 }
 
 export const projects: ProjectModel[] = [
@@ -28,7 +20,7 @@ export const projects: ProjectModel[] = [
       'The Hawbrary is a showcase of the HawAPI - A Free and Open Source API for Stranger Things - built with React (NextJs) + Typescript.',
     git: 'https://github.com/LucJosin/hawbrary',
     url: 'https://hawbrary.theproject.id/',
-    tags: ['React.js', 'Next.js', 'SDK', 'CSS', 'Git', ' TypeScript'],
+    tags: ['React.js', 'Next.js', 'SDK', 'CSS', ' TypeScript'],
     language: 'Typescript',
     icon: 'simple-icons:typescript',
     category: 'Project',
@@ -41,24 +33,10 @@ export const projects: ProjectModel[] = [
       'A Free and Open Source API for Stranger Things built with Java (Spring Boot) and PostgreSQL with support for multiple languages (English and Portuguese).',
     git: 'https://github.com/HawAPI/HawAPI',
     url: 'https://hawapi.theproject.id/',
-    tags: ['Git', 'Java', 'Spring Boot', 'JUnit', 'PostgreSQL', 'SQL', 'JSON'],
+    tags: ['Java', 'Spring Boot', 'JUnit', 'PostgreSQL', 'SQL'],
     language: 'Java',
     icon: 'mdi:language-java',
     category: 'Collection',
-    children: [
-      {
-        title: 'HawAPI/website',
-        description: 'Main website for HawAPI',
-        git: 'https://github.com/HawAPI/website',
-        url: 'https://hawapi.theproject.id/',
-      },
-      {
-        title: 'HawAPI/js-sdk',
-        description: 'HawAPI SDK for JavaScript/TypeScript',
-        git: 'https://github.com/HawAPI/js-sdk',
-        url: 'https://www.npmjs.com/package/@hawapi/js-sdk',
-      },
-    ],
   },
   {
     image:
@@ -67,7 +45,7 @@ export const projects: ProjectModel[] = [
     description:
       'Minimalist portfolio website/blog built with Astro & Typescript.',
     git: 'https://github.com/LucJosin/lucasjosino.com',
-    tags: ['Git', 'Astro', 'TypeScript', 'JavaScript', 'CSS'],
+    tags: ['Astro', 'TypeScript', 'JavaScript', 'CSS'],
     language: 'Astro',
     icon: 'simple-icons:astro',
     category: 'Project',
@@ -80,7 +58,7 @@ export const projects: ProjectModel[] = [
       'Flutter Plugin used to query audios/songs infos from device storage.',
     git: 'https://github.com/LucJosin/on_audio_query',
     url: 'https://pub.dev/packages/on_audio_query#gif-examples',
-    tags: ['Git', 'Kotlin', 'Flutter', 'Dart'],
+    tags: ['Kotlin', 'Swift', 'Flutter', 'Dart'],
     language: 'Dart',
     icon: 'simple-icons:dart',
     category: 'Project',
