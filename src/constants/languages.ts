bd0748f5de2ae7293e2c0a35bb371425154a82d1
Lucas Josino <contact@lucasjosino.com>
2024-07-25 12:51:06 -0300

    feat: add about page

---
+++ src/constants/languages.ts
@@ -0,0 +1,63 @@
+export interface Language {
+  name: string;
+  icon: string;
+}
+
+export const languages: Language[] = [
+  {
+    name: 'Java (Language)',
+    icon: 'mdi:language-java',
+  },
+  {
+    name: 'Golang (Language)',
+    icon: 'mdi:language-go',
+  },
+  {
+    name: 'Spring Boot (Backend)',
+    icon: 'simple-icons:springboot',
+  },
+  {
+    name: 'Spring Framework (Backend)',
+    icon: 'simple-icons:spring',
+  },
+  {
+    name: 'JUnit5 (Backend)',
+    icon: 'simple-icons:junit5',
+  },
+  {
+    name: 'PostgreSQL (Database)',
+    icon: 'simple-icons:postgresql',
+  },
+  {
+    name: 'MySQL (Database)',
+    icon: 'simple-icons:mysql',
+  },
+  {
+    name: 'Docker (Tool)',
+    icon: 'simple-icons:docker',
+  },
+  {
+    name: 'SQL (Backend)',
+    icon: 'tabler:file-type-sql',
+  },
+  {
+    name: 'Git (Tool)',
+    icon: 'simple-icons:git',
+  },
+  {
+    name: 'Maven (Tool)',
+    icon: 'simple-icons:apachemaven',
+  },
+  {
+    name: 'Swagger (Tool)',
+    icon: 'simple-icons:swagger',
+  },
+  {
+    name: 'Restful API (Architectural Style)',
+    icon: 'mdi:api',
+  },
+  {
+    name: 'Linux (OS)',
+    icon: 'simple-icons:linux',
+  },
+];
