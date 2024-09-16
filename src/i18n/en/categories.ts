b174276ab5ce690ec0c8373166652256691b8e90
Lucas Josino <contact@lucasjosino.com>
2024-09-16 18:21:19 -0300

    refactor: update tags and category title page

--- src/i18n/en/categories.ts
+++ src/i18n/en/categories.ts
@@ -2,7 +2,7 @@ export const categories = {
   title: 'Categories',
   description: 'Explore all categories of my personal blog',
   category: {
-    title: 'Category: {{category}}',
+    title: '{{category}} - Category',
     description: 'Explore all posts categorized with: {{category}}',
     description_one: 'Explore {{count}} post categorized with "{{category}}"',
     description_other:
--- src/i18n/en/tags.ts
+++ src/i18n/en/tags.ts
@@ -2,7 +2,7 @@ export const tags = {
   title: 'Tags',
   description: 'Explore all tags of my personal blog',
   tag: {
-    title: 'Tag: {{tag}}',
+    title: '{{tag}} - Tag',
     description: 'Explore all posts tagged with: {{tag}}',
     description_one: 'Explore {{count}} post tagged with "{{tag}}"',
     description_other: 'Explore {{count}} posts tagged with "{{tag}}"',
