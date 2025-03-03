06406f973c99156223f65d3eb4e8bfec8dc6128c
Lucas Josino <contact@lucasjosino.com>
2025-03-03 11:40:24 -0300

    refactor: add new pattern for `remark-alert-blocks`

--- plugins/remark-alert-blocks.js
+++ plugins/remark-alert-blocks.js
@@ -1,59 +1,37 @@
-import { is } from 'unist-util-is';
 import { visit } from 'unist-util-visit';
 
-const alerts = {
-  '!!!info': 'info',
-  '!!!note': 'note',
-  '!!!tip': 'tip',
-  '!!!issue': 'issue',
-  '!!!success': 'success',
-  '!!!solution': 'solution',
-  '!!!check': 'check',
-  '!!!complete': 'complete',
-  '!!!warning': 'warning',
-  '!!!error': 'error',
-  '!!!danger': 'danger',
-};
-
-const INFO_REGEX = '!!!(?:.*?)(?:\\{(.*?)(?:#(.*))?\\})?\\s(.*)';
-
 export default function remarkAlertBlocks() {
-  return function transformer(tree) {
-    visit(tree, 'paragraph', (paragraphNode, _, parent) => {
-      visit(paragraphNode, 'text', (textNode) => {
-        for (const alert in alerts) {
-          if (textNode.value.startsWith(alert)) {
-            const data = textNode.value.match(INFO_REGEX);
+  return (tree) => {
+    visit(tree, 'containerDirective', (node) => {
+      if (node.name === 'alert') {
+        const data = node.data || (node.data = {});
 
-            const id = data[1];
-            const className = data[2];
-            const text = data[3];
+        data.hName = 'div';
+        data.hProperties = {
+          className: [
+            'remark-alert-blocks',
+            `remark-alert-${node.attributes?.type}`,
+            node.attributes?.className,
+          ],
+          id: node.attributes?.id,
+        };
 
-            textNode.value = text;
-            parent.children = parent.children.map((node) => {
-              if (is(paragraphNode, node)) {
-                return {
-                  type: 'wrapper',
-                  children: [node],
-                  data: {
-                    hName: 'div',
-                    hProperties: {
-                      id,
-                      className: [
-                        'remark-alerts',
-                        `remark-alert-${alerts[alert]}`,
-                        className,
-                      ],
-                    },
-                  },
-                };
-              }
+        const alertContent = {
+          type: 'element',
+          data: {
+            hName: 'div',
+            hProperties: {
+              className: [
+                'remark-alert-content',
+                `remark-alert-${node.attributes?.type}-content`,
+              ],
+            },
+          },
+          children: node.children,
+        };
 
-              return node;
-            });
-          }
-        }
-      });
+        node.children = [alertContent];
+      }
     });
   };
 }
